1d0d90f52be88310c15b98b0137f33ab8d88142b
rawwr <akshayace0302@gmail.com>
2022-11-06 19:15:55 +0530

    the post detail page

---
+++ components/posts/post-detail/post-content.tsx
@@ -0,0 +1,22 @@
+import PostHeader from "./post-header";
+import classes from "./post-content.module.css";
+
+const DUMMY_POSTS = {
+  slug: "getting-started-with-nextjs",
+  title: "Getting Started with NextJS",
+  image: "nextjs.png",
+  date: "2022-02-10",
+  content: "# HI there this is akshay here",
+};
+
+const PostContent = () => {
+  const imagePath = `/images/posts/${DUMMY_POSTS.slug}/${DUMMY_POSTS.image}`;
+  return (
+    <article className={classes.content}>
+      <PostHeader title={DUMMY_POSTS.title} image={imagePath} />
+      {DUMMY_POSTS.content}
+    </article>
+  );
+};
+
+export default PostContent;
