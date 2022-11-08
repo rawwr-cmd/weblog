65966a3d78314139a8c7799afb9da7f5bcfdb755
rawwr <akshayace0302@gmail.com>
2022-11-09 01:05:36 +0530

    adding ui feedback notification

--- components/contact/contact-form.tsx
+++ components/contact/contact-form.tsx
@@ -1,30 +1,98 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import classes from "./contact-form.module.css";
+import Notification from "../ui/notification";
+
+interface sendDataProps {
+  email: string;
+  name: string;
+  message: string;
+}
+
+const sendContactData = async (contactDetails: sendDataProps) => {
+  const response = await fetch("/api/contact-api", {
+    method: "POST",
+    //Converts a JavaScript value to a JavaScript Object Notation (JSON) string.
+    body: JSON.stringify(contactDetails),
+    //The MIME type of the body of the request (used by the server to determine how to process the request).
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error(data.message || "Something went wrong!");
+  }
+};
 
 const ContactForm = () => {
   const [enteredEmail, setEnteredEmail] = useState<string>("");
   const [enteredName, setEnteredName] = useState<string>("");
   const [enteredMessage, setEnteredMessage] = useState<string>("");
+  const [requestStatus, setRequestStatus] = useState<string>(""); //pending, success, error
+  const [requestError, setRequestError] = useState<string>("");
 
-  const sendMessageHandler = (event: React.FormEvent) => {
+  useEffect(() => {
+    if (requestStatus === "success" || requestStatus === "error") {
+      const timer = setTimeout(() => {
+        setRequestStatus("");
+        setRequestError("");
+      }, 3000);
+
+      //cleanUpFunction
+      return () => clearTimeout(timer);
+    }
+  }, [requestStatus]);
+
+  const sendMessageHandler = async (event: React.FormEvent) => {
     event.preventDefault();
 
     //add client-side validation
 
-    fetch("/api/contact-api", {
-      method: "POST",
-      //Converts a JavaScript value to a JavaScript Object Notation (JSON) string.
-      body: JSON.stringify({
+    setRequestStatus("pending");
+
+    try {
+      await sendContactData({
         email: enteredEmail,
         name: enteredName,
         message: enteredMessage,
-      }),
-      //The MIME type of the body of the request (used by the server to determine how to process the request).
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+      });
+      setRequestStatus("success");
+      setEnteredMessage("");
+      setEnteredEmail("");
+      setEnteredName("");
+    } catch (error: any) {
+      setRequestError(error.message);
+      setRequestStatus("error");
+    }
   };
+
+  let notification;
+
+  if (requestStatus === "pending") {
+    notification = {
+      title: "Sending message...",
+      message: "Your message is on its way!",
+      status: "pending",
+    };
+  }
+
+  if (requestStatus === "success") {
+    notification = {
+      title: "Success!",
+      message: "Message sent successfully!",
+      status: "success",
+    };
+  }
+
+  if (requestStatus === "error") {
+    notification = {
+      title: "Error!",
+      message: "Message failed to send!",
+      status: requestError,
+    };
+  }
 
   return (
     <section className={classes.contact}>
@@ -66,6 +134,13 @@ const ContactForm = () => {
           <button>Send Message</button>
         </div>
       </form>
+      {notification && (
+        <Notification
+          title={notification.title}
+          message={notification.message}
+          status={notification.status}
+        />
+      )}
     </section>
   );
 };
