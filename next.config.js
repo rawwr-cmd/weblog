c75572582d522e4c5490ddd595cc5518a0a82455
rawwr <akshayace0302@gmail.com>
2022-11-10 23:58:38 +0530

    setting up env variables

--- next.config.js
+++ next.config.js
@@ -1,7 +1,29 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+const { PHASE_DEVLOPMENT_SERVER } = require("next/constants");
 
-module.exports = nextConfig
+const nextConfig = (phase) => {
+  if (phase === PHASE_DEVLOPMENT_SERVER) {
+    return {
+      reactStrictMode: true,
+      swcMinify: true,
+      env: {
+        mongodb_username: "rawwr",
+        mongodb_password: "kCIexrkEqPj0UFbf",
+        mongodb_clustername: "cluster0",
+        mongodb_database: "my-blog-dev",
+      },
+    };
+  }
+  return {
+    reactStrictMode: true,
+    swcMinify: true,
+    env: {
+      mongodb_username: "rawwr",
+      mongodb_password: "kCIexrkEqPj0UFbf",
+      mongodb_clustername: "cluster0",
+      mongodb_database: "my-blog",
+    },
+  };
+};
+
+module.exports = nextConfig;
--- pages/api/contact-api.tsx
+++ pages/api/contact-api.tsx
@@ -35,10 +35,10 @@ const Handler = async (req: NextApiRequest, res: NextApiResponse) => {
 
     let client;
 
+    const connectionString = `mongodb+srv://${process.env.mongodb_username}:${process.env.mongodb_password}@${process.env.mongodb_clustername}.eyknml1.mongodb.net/${process.env.mongodb_database}?retryWrites=true&w=majority`;
+
     try {
-      client = await MongoClient.connect(
-        "mongodb+srv://rawwr:kCIexrkEqPj0UFbf@cluster0.eyknml1.mongodb.net/my-blog?retryWrites=true&w=majority"
-      );
+      client = await MongoClient.connect(connectionString);
     } catch (error) {
       res.status(500).json({ message: "Could not connect to database." });
       return;
